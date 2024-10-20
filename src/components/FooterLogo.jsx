94fb754e3452a02d37808734fed2acfc7e7bab9f
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-20 12:07:37 +0500

    Create Footer logo Component

---
+++ src/components/FooterLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const FooterLogo = () => {
+	return (
+		<div>
+			<img
+				className="h-8"
+				src="https://raw.githubusercontent.com/bradtraversy/tailwind-landing-page/refs/heads/main/img/logo-white.svg"
+				alt="logo-white"
+			/>
+		</div>
+	);
+};
