0646ef26727ad79ceb32b47303e67f4f4709d1e6
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-20 07:07:11 +0500

    Create Hero Image component

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HeroImage = () => {
+	return (
+		<div className="max-w-1/2">
+			<img
+				src="https://raw.githubusercontent.com/bradtraversy/tailwind-landing-page/refs/heads/main/img/illustration-intro.svg"
+				alt=""
+			/>
+		</div>
+	);
+};
