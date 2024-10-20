54af45a72322c8556edb66b1dfa5498d28d4e6e7
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-20 12:07:19 +0500

    Create Copyright Component

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Copyright = () => {
+	return (
+		<div className="mx-auto mb-40 mt-10 text-center text-white md:hidden">
+			Copyright &copy; 2024, All Right reserved
+		</div>
+	);
+};
