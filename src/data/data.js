f622198f3fef9d4cd8ab66c94ff81f62fcd7a7cc
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-20 08:36:07 +0500

    Add data for testimonial section

--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,32 @@ export const navItems = [
 	"About us",
 	"Careers",
 	"Community",
-	"Contact us"
+	"Contact us",
 ];
 
+export const heroDiscription =
+	"Manages makes it simple for software team to build day-to-day products while keeping the larger team goals in view.";
 
-export const heroDiscription = 'Manages makes it simple for software team to build day-to-day products while keeping the larger team goals in view.'
+export const testimonialDiscription =
+	"Manage provides all the functionality the team needs without the complexity. Our software is tailor-made for digital teams.";
+
+export const testimonialDiscriptionItemsData = [
+	{
+		number: "01",
+		title: "Track company-wide progress ",
+		discription:
+			"See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way down to the smallest of details. Never lose sight of the bigger picture again. ",
+	},
+	{
+		number: "02",
+		title: "Advanced built-in reports",
+		discription:
+			"et internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed. ",
+	},
+	{
+		number: "03",
+		title: "Everything you need in one place ",
+		discription:
+			"Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution. ",
+	},
+];
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Nav } from "./components/Nav";
 import { Hero } from "./components/Hero";
+import { Testimonial } from "./components/Testimonial";
+
 
 import "./index.css";
 
@@ -9,5 +11,6 @@ createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Nav />
 		<Hero />
+    <Testimonial />
 	</StrictMode>
 );
